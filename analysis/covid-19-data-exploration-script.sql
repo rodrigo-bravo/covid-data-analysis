5e9d2a28750807b8a819c17db095bea6b42669d7
Rodrigo Bravo <89810908+rodrigo-bravo@users.noreply.github.com>
2022-04-07 16:39:51 -0400

    Update

--- analysis/covid-19-data-exploration-script.sql
+++ analysis/covid-19-data-exploration-script.sql
@@ -10,7 +10,6 @@ order by 3,4
 
 
 -- Select Data that we are going to be starting with
-
 Select Location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProject..CovidDeaths
 Where continent is not null 
@@ -19,7 +18,6 @@ order by 1,2
 
 -- Total Cases vs Total Deaths
 -- Shows likelihood of dying if you contract covid in your country
-
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
 Where location like '%states%'
@@ -29,37 +27,26 @@ order by 1,2
 
 -- Total Cases vs Population
 -- Shows what percentage of population infected with Covid
-
 Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
 order by 1,2
 
 
 -- Countries with Highest Infection Rate compared to Population
-
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
 Group by Location, Population
 order by PercentPopulationInfected desc
 
-
 -- Countries with Highest Death Count per Population
-
 Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
 Where continent is not null 
 Group by Location
 order by TotalDeathCount desc
 
 
-
--- BREAKING THINGS DOWN BY CONTINENT
-
 -- Showing contintents with the highest death count per population
-
 Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
 --Where location like '%states%'
@@ -70,19 +57,15 @@ order by TotalDeathCount desc
 
 
 -- GLOBAL NUMBERS
-
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
 where continent is not null 
---Group By date
 order by 1,2
 
 
 
 -- Total Population vs Vaccinations
 -- Shows Percentage of Population that has recieved at least one Covid Vaccine
-
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population)*100
@@ -95,7 +78,6 @@ order by 2,3
 
 
 -- Using CTE to perform Calculation on Partition By in previous query
-
 With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
 as
 (
@@ -115,7 +97,6 @@ From PopvsVac
 
 
 -- Using Temp Table to perform Calculation on Partition By in previous query
-
 DROP Table if exists #PercentPopulationVaccinated
 Create Table #PercentPopulationVaccinated
 (
@@ -145,7 +126,6 @@ From #PercentPopulationVaccinated
 
 
 -- Creating View to store data for later visualizations
-
 Create View PercentPopulationVaccinated as
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
